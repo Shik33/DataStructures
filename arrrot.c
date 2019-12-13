b94a11849cf2b2c6c8a80ee56ce56bb875fdac7d
shikhir.varma <shikhir.varma@sevenproper.com>
2019-12-13 21:12:32 +0530

    Rot

---
+++ arrrot.c
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void rotation(int arr[], int n)
+{
+	int temp, i;
+
+	temp = arr[0];
+	i = 0;
+	while (i < n - 1)
+	{
+		arr[i] = arr[i + 1];
+		i++;
+	}
+	arr[i] = temp;
+}
+
+void rotationford(int a[], int n, int d)
+{
+	int i;
+
+	i = 0;
+	while (i < d)
+	{
+		rotation(a, n);
+		i++;
+	}
+}
+
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int i;
+
+	i = 0;
+	rotationford(arr, n, 2);
+	while (i < n)
+	{
+		cout << arr[i] << "\n";
+		i++;
+	}
+	return (0);
+}
